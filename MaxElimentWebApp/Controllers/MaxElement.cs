0239b05b3f84f6b02642a079172187746844cef5
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-12-11 21:51:32 +0530

    Add project files.

---
+++ MaxElimentWebApp/Controllers/MaxElement.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaxElimentWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaxElement : ControllerBase
+    {
+        [HttpGet(Name = "getMaxElementNumber")]
+
+        public IActionResult getMaxElementNumber([FromQuery] int Number1, [FromQuery] int Number2)
+        {
+            return Ok("MaxElement Number1:" + Number1 + "and Number2:" + Number2);
+        }
+    }
+}
